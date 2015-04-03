00eac6839315d0ae704d8fbb457d41d562949918
Matthew Chambers <matthew@chambers.net.nz>
2015-04-04 01:44:38 +1300

    Start modeling header table lookups

--- http2/hpack.fs
+++ http2/hpack.fs
@@ -1,84 +1,124 @@
 ﻿namespace http2
 
+open System
 open System.Collections.Generic
 
 module hpack =
+    
+    type IndexingAction =
+        | Incremental
+        | NonIndexed
+
+    type LiteralHeaderKind =
+        | IndexedName of IndexingAction * int * string
+        | NewName of IndexingAction * string * string
+
     type HeaderRepresentation = 
-        | Literal of byte array
-        | Index of int
+        | LiteralHeader of LiteralHeaderKind
+        | IndexedHeader of int
 
     type EncodedHeaderBlock = HeaderRepresentation list
 
-    type StaticTableEntry = {
+    type HeaderTableEntry = {
         Name: string;
-        Value: string option
+        Value: string
     }
 
-                     
+    let (|Static|Dynamic|Error|) index = 
+        if index <= 0 then
+            Error
+        else if index <= 61 then
+            Static
+        else
+            Dynamic
+        
 
     let staticHeaderTable =
         [
-            1, {Name = ":authority"; Value = None};
-            2, {Name = ":method"; Value = Some "GET"};
-            3, {Name = ":method"; Value = Some "POST"};
-            4, {Name = ":path"; Value = Some "/"};
-            5, {Name = ":path"; Value = Some "/index.html"};
-            6, {Name = ":scheme"; Value = Some "http"};
-            7, {Name = ":scheme"; Value = Some "https"};
-            8, {Name = ":status"; Value = Some "200"};
-            9, {Name = ":status"; Value = Some "204"};
-            10, {Name = ":status"; Value = Some "206"};
-            11, {Name = ":status"; Value = Some "304"};                                
-            12, {Name = ":status"; Value = Some "400"};
-            13, {Name = ":status"; Value = Some "404"};                                 
-            14, {Name = ":status"; Value = Some "500"};                                 
-            15, {Name = "accept-charset"; Value = None};                   
-            16, {Name = "accept-encoding"; Value = None};                  
-            17, {Name = "accept-language"; Value = None};                  
-            18, {Name = "accept-ranges"; Value = None};                    
-            19, {Name = "accept"; Value = None};                           
-            20, {Name = "access-control-allow-origin"; Value = None};      
-            21, {Name = "age"; Value = None};                              
-            22, {Name = "allow"; Value = None};                            
-            23, {Name = "authorization"; Value = None};                    
-            24, {Name = "cache-control"; Value = None};                    
-            25, {Name = "content-disposition"; Value = None};              
-            26, {Name = "content-encoding"; Value = None};                 
-            27, {Name = "content-language"; Value = None};                 
-            28, {Name = "content-length"; Value = None};                   
-            29, {Name = "content-location"; Value = None};                 
-            30, {Name = "content-range"; Value = None};                    
-            31, {Name = "content-type"; Value = None};                      
-            32, {Name = "cookie"; Value = None};                           
-            33, {Name = "date"; Value = None};                             
-            34, {Name = "etag"; Value = None};                              
-            35, {Name = "expect"; Value = None};                           
-            36, {Name = "expires"; Value = None};                          
-            37, {Name = "from"; Value = None};                             
-            38, {Name = "host"; Value = None};                             
-            39, {Name = "if-match"; Value = None};                         
-            40, {Name = "if-modified-since"; Value = None};                
-            41, {Name = "if-none-match"; Value = None};                    
-            42, {Name = "if-range"; Value = None};                         
-            43, {Name = "if-unmodified-since"; Value = None};              
-            44, {Name = "last-modified"; Value = None};                    
-            45, {Name = "link"; Value = None};                             
-            46, {Name = "location"; Value = None};                         
-            47, {Name = "max-forwards"; Value = None};                     
-            48, {Name = "proxy-authenticate"; Value = None};               
-            49, {Name = "proxy-authorization"; Value = None};              
-            50, {Name = "range"; Value = None};                            
-            51, {Name = "referer"; Value = None};                          
-            52, {Name = "refresh"; Value = None};                          
-            53, {Name = "retry-after"; Value = None};                      
-            54, {Name = "server"; Value = None};                           
-            55, {Name = "set-cookie"; Value = None};                       
-            56, {Name = "strict-transport-security"; Value = None};        
-            57, {Name = "transfer-encoding"; Value = None};                
-            58, {Name = "user-agent"; Value = None};                       
-            59, {Name = "vary"; Value = None};                             
-            60, {Name = "via"; Value = None};                              
-            61, {Name = "www-authenticate"; Value = None};
+            1, {Name = ":authority"; Value = ""};
+            2, {Name = ":method"; Value = "GET"};
+            3, {Name = ":method"; Value = "POST"};
+            4, {Name = ":path"; Value = "/"};
+            5, {Name = ":path"; Value = "/index.html"};
+            6, {Name = ":scheme"; Value = "http"};
+            7, {Name = ":scheme"; Value = "https"};
+            8, {Name = ":status"; Value = "200"};
+            9, {Name = ":status"; Value = "204"};
+            10, {Name = ":status"; Value = "206"};
+            11, {Name = ":status"; Value = "304"};                                
+            12, {Name = ":status"; Value = "400"};
+            13, {Name = ":status"; Value = "404"};                                 
+            14, {Name = ":status"; Value = "500"};
+            15, {Name = "accept-charset"; Value = ""};
+            16, {Name = "accept-encoding"; Value = ""};
+            17, {Name = "accept-language"; Value = ""};
+            18, {Name = "accept-ranges"; Value = ""};               
+            19, {Name = "accept"; Value = ""};
+            20, {Name = "access-control-allow-origin"; Value = ""};
+            21, {Name = "age"; Value = ""};
+            22, {Name = "allow"; Value = ""};
+            23, {Name = "authorization"; Value = ""};
+            24, {Name = "cache-control"; Value = ""};
+            25, {Name = "content-disposition"; Value = ""};
+            26, {Name = "content-encoding"; Value = ""};
+            27, {Name = "content-language"; Value = ""};
+            28, {Name = "content-length"; Value = ""};
+            29, {Name = "content-location"; Value = ""};
+            30, {Name = "content-range"; Value = ""};
+            31, {Name = "content-type"; Value = ""};
+            32, {Name = "cookie"; Value = ""};
+            33, {Name = "date"; Value = ""};
+            34, {Name = "etag"; Value = ""};
+            35, {Name = "expect"; Value = ""};
+            36, {Name = "expires"; Value = ""};
+            37, {Name = "from"; Value = ""};
+            38, {Name = "host"; Value = ""};
+            39, {Name = "if-match"; Value = ""};
+            40, {Name = "if-modified-since"; Value = ""};
+            41, {Name = "if-none-match"; Value = ""};
+            42, {Name = "if-range"; Value = ""};
+            43, {Name = "if-unmodified-since"; Value = ""};
+            44, {Name = "last-modified"; Value = ""};
+            45, {Name = "link"; Value = ""};
+            46, {Name = "location"; Value = ""};
+            47, {Name = "max-forwards"; Value = ""};
+            48, {Name = "proxy-authenticate"; Value = ""};
+            49, {Name = "proxy-authorization"; Value = ""};
+            50, {Name = "range"; Value = ""};
+            51, {Name = "referer"; Value = ""};
+            52, {Name = "refresh"; Value = ""};
+            53, {Name = "retry-after"; Value = ""};
+            54, {Name = "server"; Value = ""};
+            55, {Name = "set-cookie"; Value = ""};
+            56, {Name = "strict-transport-security"; Value = ""};
+            57, {Name = "transfer-encoding"; Value = ""};
+            58, {Name = "user-agent"; Value = ""};
+            59, {Name = "vary"; Value = ""};
+            60, {Name = "via"; Value = ""};
+            61, {Name = "www-authenticate"; Value = ""};
         ]
         |> Map.ofList
+
+    type DynamicHeaderTable = {
+        entries: (string * string) list
+    }
         
+    let decodeHeaderList (headers: HeaderRepresentation list) =
+        let decodeHeader header = 
+            match header with
+            | LiteralHeader data -> 
+                match data with
+                | IndexedName(_, tblIdx, v) -> 
+                    match tblIdx with
+                    | Static -> {Name = ""; Value = v}
+                    | Dynamic -> {Name = ""; Value = v}
+                    | Error -> failwith ""
+                | NewName(_, n, v) -> {Name = n; Value = v}
+
+            | IndexedHeader index -> 
+                let header = staticHeaderTable.TryFind index
+                match header with
+                | None -> failwith "Could not find indexed value in static table. TODO: lookup in dynamic table."
+                | Some h -> h
+        headers
+        |> List.map decodeHeader
