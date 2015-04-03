67dcdeecd1755922546325b57d1893d54a16e5c8
Matthew Chambers <matthew@chambers.net.nz>
2015-04-04 00:08:21 +1300

    Initial Commit

---
+++ http2/Library1.fs
@@ -0,0 +1,44 @@
+﻿namespace http2
+
+module ssl =
+    open System.Net.Sockets
+    open System.Net.Security
+    open System.Net
+    open System.Net.Http
+    open System.Security.Authentication
+    open System.IO
+    open FSharp.Control.Reactive
+    open Builders
+    open System.Security.Cryptography.X509Certificates
+    open System
+
+    type SslServer = {
+        listener: TcpListener;
+        incomingConnections: IObservable<SslStream>
+    }
+    
+    let acceptConnection (listener: TcpListener) (cert: X509Certificate) =
+        let client = listener.AcceptTcpClient()
+        let stream = client.GetStream()
+        let sslStream = new SslStream(stream, false)
+        sslStream.AuthenticateAsServer(cert, false, SslProtocols.Default, true)
+        sslStream
+
+    let createServer port cert = 
+        let listener = new TcpListener(IPAddress.Any, port)
+        let incomingConnections = observe {
+            while true do
+                yield acceptConnection listener cert
+        }
+        {listener = listener; 
+        incomingConnections = incomingConnections}
+
+    type Http2Request = {
+        Method: HttpMethod
+    }
+
+    let processHttp2Connection (sslStream: SslStream) : IObservable<Http2Request> =
+        observe {
+            yield {Method = HttpMethod.Get}
+        }
+
---
+++ http2/hpack.fs
@@ -0,0 +1,84 @@
+﻿namespace http2
+
+open System.Collections.Generic
+
+module hpack =
+    type HeaderRepresentation = 
+        | Literal of byte array
+        | Index of int
+
+    type EncodedHeaderBlock = HeaderRepresentation list
+
+    type StaticTableEntry = {
+        Name: string;
+        Value: string option
+    }
+
+                     
+
+    let staticHeaderTable =
+        [
+            1, {Name = ":authority"; Value = None};
+            2, {Name = ":method"; Value = Some "GET"};
+            3, {Name = ":method"; Value = Some "POST"};
+            4, {Name = ":path"; Value = Some "/"};
+            5, {Name = ":path"; Value = Some "/index.html"};
+            6, {Name = ":scheme"; Value = Some "http"};
+            7, {Name = ":scheme"; Value = Some "https"};
+            8, {Name = ":status"; Value = Some "200"};
+            9, {Name = ":status"; Value = Some "204"};
+            10, {Name = ":status"; Value = Some "206"};
+            11, {Name = ":status"; Value = Some "304"};                                
+            12, {Name = ":status"; Value = Some "400"};
+            13, {Name = ":status"; Value = Some "404"};                                 
+            14, {Name = ":status"; Value = Some "500"};                                 
+            15, {Name = "accept-charset"; Value = None};                   
+            16, {Name = "accept-encoding"; Value = None};                  
+            17, {Name = "accept-language"; Value = None};                  
+            18, {Name = "accept-ranges"; Value = None};                    
+            19, {Name = "accept"; Value = None};                           
+            20, {Name = "access-control-allow-origin"; Value = None};      
+            21, {Name = "age"; Value = None};                              
+            22, {Name = "allow"; Value = None};                            
+            23, {Name = "authorization"; Value = None};                    
+            24, {Name = "cache-control"; Value = None};                    
+            25, {Name = "content-disposition"; Value = None};              
+            26, {Name = "content-encoding"; Value = None};                 
+            27, {Name = "content-language"; Value = None};                 
+            28, {Name = "content-length"; Value = None};                   
+            29, {Name = "content-location"; Value = None};                 
+            30, {Name = "content-range"; Value = None};                    
+            31, {Name = "content-type"; Value = None};                      
+            32, {Name = "cookie"; Value = None};                           
+            33, {Name = "date"; Value = None};                             
+            34, {Name = "etag"; Value = None};                              
+            35, {Name = "expect"; Value = None};                           
+            36, {Name = "expires"; Value = None};                          
+            37, {Name = "from"; Value = None};                             
+            38, {Name = "host"; Value = None};                             
+            39, {Name = "if-match"; Value = None};                         
+            40, {Name = "if-modified-since"; Value = None};                
+            41, {Name = "if-none-match"; Value = None};                    
+            42, {Name = "if-range"; Value = None};                         
+            43, {Name = "if-unmodified-since"; Value = None};              
+            44, {Name = "last-modified"; Value = None};                    
+            45, {Name = "link"; Value = None};                             
+            46, {Name = "location"; Value = None};                         
+            47, {Name = "max-forwards"; Value = None};                     
+            48, {Name = "proxy-authenticate"; Value = None};               
+            49, {Name = "proxy-authorization"; Value = None};              
+            50, {Name = "range"; Value = None};                            
+            51, {Name = "referer"; Value = None};                          
+            52, {Name = "refresh"; Value = None};                          
+            53, {Name = "retry-after"; Value = None};                      
+            54, {Name = "server"; Value = None};                           
+            55, {Name = "set-cookie"; Value = None};                       
+            56, {Name = "strict-transport-security"; Value = None};        
+            57, {Name = "transfer-encoding"; Value = None};                
+            58, {Name = "user-agent"; Value = None};                       
+            59, {Name = "vary"; Value = None};                             
+            60, {Name = "via"; Value = None};                              
+            61, {Name = "www-authenticate"; Value = None};
+        ]
+        |> Map.ofList
+        
