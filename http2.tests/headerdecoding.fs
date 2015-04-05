9d754f18eebb1d7c525e20b5619f2b08e3bb6dcf
Matthew Chambers <matthew@chambers.net.nz>
2015-04-05 18:55:52 +1200

    Add more dynamic table tests

--- http2.tests/headerdecoding.fs
+++ http2.tests/headerdecoding.fs
@@ -56,6 +56,73 @@ let ``Processing header with Incremental indexing updates dynamic table``() =
         LiteralHeader (
             NewName (
                 Incremental, "NewName", "NewValue"))
+    
     let _, newTable = processHeader header emptyDynamicTable
 
-    newTable |> should not' (equal emptyDynamicTable)
+    let expectedNewTable = {
+        emptyDynamicTable 
+        with entries = [{Name = "NewName"; Value = "NewValue"}]
+    }
+
+    newTable |> should equal expectedNewTable
+
+[<Test>]
+let ``Processing two header with Incremental indexing updates dynamic table to hold both headers``() =
+    let header = 
+        LiteralHeader (
+            NewName (
+                Incremental, "NewName", "NewValue"))
+    
+    let _, newTable1 = processHeader header emptyDynamicTable
+    let _, newTable2 = processHeader header newTable1
+
+    let expectedNewTable = 
+        {
+            emptyDynamicTable 
+            with entries = [{Name = "NewName"; Value = "NewValue"}; {Name = "NewName"; Value = "NewValue"}]
+        }
+
+    newTable2 |> should equal expectedNewTable
+
+[<Test>]
+let ``Processing two headers with Incremental indexing on full dynamic table evicts oldest members``() =
+    let h1 = 
+        LiteralHeader (
+            NewName (
+                Incremental, "0987654321", "0987654321"))    
+    let h2 = 
+        LiteralHeader (
+            NewName (
+                Incremental, "1234567890", "1234567890"))
+
+    let smallDynamicTable = { maxSize= 60; entries = [] }
+    
+    let _, newTable1 = processHeader h1 smallDynamicTable
+    let _, newTable2 = processHeader h2 newTable1
+
+    let expectedNewTable = 
+        {
+            maxSize = 60; 
+            entries = [{Name = "1234567890"; Value = "1234567890"}]
+        }
+
+    newTable2 |> should equal expectedNewTable
+
+[<Test>]
+let ``Inserting too large a header empties the dynamic header table``() =
+    let h1 = 
+        LiteralHeader (
+            NewName (
+                Incremental, "098765432109876543210987654321", "098765432109876543210987654321")) 
+                   
+    let smallDynamicTable = { maxSize = 60; entries = [{Name = "1234567890"; Value = "1234567890"}] }
+    
+    let _, newTable = processHeader h1 smallDynamicTable
+
+    let expectedNewTable = 
+        {
+            maxSize = 60; 
+            entries = []
+        }
+
+    newTable |> should equal expectedNewTable
